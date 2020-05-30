84152079a569161acca0e350de5536814b8dc30d
Josef Nemec <nemecjosef@gmail.com>
2020-05-30 19:35:30 +0200

    GTA 5 not starting properly

--- source/RockstartGames.cs
+++ source/RockstartGames.cs
@@ -24,7 +24,7 @@ namespace RockstarGamesLibrary
             new RockstarGame
             {
                 Name = "Grand Theft Auto V",
-                Executable = "GTA5.exe",
+                Executable = "PlayGTAV.exe",
                 TitleId = "gta5"
             },
             new RockstarGame
